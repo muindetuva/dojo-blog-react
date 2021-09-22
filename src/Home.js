56dff11d7e58efc8f7106a7554c8936a04e7d6b3
Alfred Tuva <muindetuva@gmail.com>
2021-09-22 18:50:45 +0300

    Use props to pass data from parent blog to a nested blog

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -24,12 +25,8 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      {/* Use props to pass data */}
+      <BlogList blogs={blogs} title="All blogs!" />
     </div>
   );
 };
