0ec946837fa413753b5a5f4bd587ccedefbe4bdd
Alfred Tuva <muindetuva@gmail.com>
2021-09-22 18:35:27 +0300

    Display blog posts

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "Lorem Ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome to the party",
+      body: "Lorem Ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Top tobs",
+      body: "Lorem Ipsum...",
+      author: "luigi",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
