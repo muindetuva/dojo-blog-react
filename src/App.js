26960e5ea27f5373c10095a24e605ae3a71aaa52
Alfred Tuva <muindetuva@gmail.com>
2021-09-30 16:13:58 +0300

    Add dynamic links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
             </Route>
             <Route exact path="/create">
               <Create />
+            </Route>
+            <Route exact path="/blog/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 const BlogList = ({ blogs, title }) => {
   //   const blogs = props.blogs;
   //   const title = props.title;
@@ -8,8 +9,10 @@ const BlogList = ({ blogs, title }) => {
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blog/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
