59245e6e60c24f87567e5982244d278278d3e647
Alfred Tuva <muindetuva@gmail.com>
2021-09-30 15:53:45 +0300

    Add routes

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import React from 'react';
 const BlogList = ({ blogs, title }) => {
   //   const blogs = props.blogs;
   //   const title = props.title;
--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,17 @@
-import { useState } from "react";
+import React from 'react';
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My new website",
-      body: "Lorem Ipsum...",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Welcome to the party",
-      body: "Lorem Ipsum...",
-      author: "yoshi",
-      id: 2,
-    },
-    {
-      title: "Top tobs",
-      body: "Lorem Ipsum...",
-      author: "luigi",
-      id: 3,
-    },
-  ]);
+
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
       {/* Use props to pass data */}
-      <BlogList blogs={blogs} title="All blogs!" />
+      {error && <div> {error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
 };
